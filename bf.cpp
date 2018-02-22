edf496ad3238bd473d595292b6ff01450e870a05
Sergey <sergey.abramov@gmail.com>
2018-02-22 12:09:31 +0700

    Add some functions

--- bf.cpp
+++ bf.cpp
@@ -1,5 +1,31 @@
-#include "bf.h"
+#include <iostream>
+#include <string>
+#include <assert.h>
+#include <bf.h>
 
-bf::bf()
+bf::bf() : m_func (NULL), m_var(0), m_len(0) {}
+
+bf::bf(std::string str)
 {
+    assert(!str.empty());
+
+    for (unsigned int i = 0; i < str.length(); i++){
+
+    }
+
 }
+
+bf::~bf()
+{
+    if (m_func) delete [] m_func;
+}
+
+//unsigned int bf::lenFunc()
+//{
+//    unsigned int countComb = 2 << (m_var - 1);
+//    unsigned int whole = countComb / NUM_BIT_IN_BASE;
+//    unsigned int remainder = countComb / NUM_BIT_IN_BASE;
+
+//    if (remainder != 0) return whole++;
+//    else return whole;
+//}
--- bf.h
+++ bf.h
@@ -1,16 +1,26 @@
 #ifndef BF_H
 #define BF_H
 
+typedef unsigned int Base;
+const unsigned int NUM_BIT_IN_BASE = 8*sizeof(Base);
+
 class bf
 {
 private:
-    unsigned int m_var;
+    Base *m_func; // value of function
 
+    Base m_var; // number of variable. MAX m_var = 31
+
+    Base m_len;
 
 public:
-    bf();
+    bf ();
 
+    bf (std::string str);
 
+    ~bf ();
+
+//    unsigned int lenFunc(); // may not fit in "int"
 };
 
 #endif // BF_H
