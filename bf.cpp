4823e9a32c198865612d7df124a192e1f441591f
AbrSergey <sergey.abramov996@gmail.com>
2018-03-01 13:45:47 +0700

    Modify bf(string str)

--- bf.cpp
+++ bf.cpp
@@ -34,7 +34,7 @@ bf::bf(std::string str)
     // write str to m_func
     for (int i = 0; i < whole; i++)
         for (int j = 0; j < NUM_BIT_IN_BASE; j++){
-            char ch = str[i * NUM_BIT_IN_BASE + j];
+            char ch = str[lenStr - (i * NUM_BIT_IN_BASE + j) - 1];
             assert(ch == '0' || ch == '1');
 
             if (ch == '1') m_func[i] |= 1;
@@ -43,7 +43,7 @@ bf::bf(std::string str)
 
     if (remainder != 0)
         for (int j = 0; j < remainder; j++){
-            char ch = str[whole * NUM_BIT_IN_BASE + j];
+            char ch = str[lenStr - (whole * NUM_BIT_IN_BASE + j) - 1];
             assert(ch == '0' || ch == '1');
 
             if (ch == '1') m_func[whole] |= 1;
@@ -59,8 +59,8 @@ bf::~bf()
 
 void bf::print() const
 {
-    std::cout << "m_var = " << m_var << std::endl;
     std::cout << "m_len = " << m_len << std::endl;
+    std::cout << "m_var = " << m_var << std::endl;
     std::cout << "m_func = ";
 
     int whole = (2 << (m_var - 1)) / NUM_BIT_IN_BASE;
@@ -70,8 +70,8 @@ void bf::print() const
     for (int i = 0; i < whole; i++){
         funcHelper = m_func[i];
         for (int j = 0; j < NUM_BIT_IN_BASE; j++){
-            if ((funcHelper & 1) == 1) std::cout << "1 ";
-            else std::cout << "0 ";
+            if ((funcHelper & 1) == 1) std::cout << "1";
+            else std::cout << "0";
 
             funcHelper >>= 1;
         }
@@ -80,8 +80,8 @@ void bf::print() const
     if (remainder != 0){
         funcHelper = m_func[m_len - 1];
         for (int j = 0; j < remainder; j++){
-            if ((funcHelper & 1) == 1) std::cout << "1 ";
-            else std::cout << "0 ";
+            if ((funcHelper & 1) == 1) std::cout << "1";
+            else std::cout << "0";
 
             funcHelper >>= 1;
         }
